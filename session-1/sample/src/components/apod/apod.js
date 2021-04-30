9ccd71f6a8960431b66b5a54ec0250be02ec7f4c
darkhorrow <benearosemidan@gmail.com>
2021-04-30 08:37:58 +0100

    Partial solution with componentDidUpdate

--- session-1/sample/src/components/apod/apod.js
+++ session-1/sample/src/components/apod/apod.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
-import Card from 'react-bootstrap/Card'
+import CardShow from './cardshow';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import './apod.css';
 import Alert from '../alert';
-import Video from '../video';
-import { API_KEY } from '../../config';
 
 export default class Apod extends Component {
     constructor(props) {
@@ -13,42 +11,8 @@ export default class Apod extends Component {
     
         this.state = {
             apod: {},
-            dateSelected: new Date(),
-            error: {
-                isActive: false,
-                message: ''
-            }
+            dateSelected: new Date()
         }
-    }
-
-    componentDidMount() {
-        fetch('https://api.nasa.gov/planetary/apod?api_key=' + API_KEY)
-            .then(response => response.json())
-            .then(data => {this.setState({apod: data})});
-    }
-
-    handleDateChange(date) {
-        const url = 'https://api.nasa.gov/planetary/apod?api_key=' + API_KEY + '&date=' + 
-            date.getFullYear() + '-' + (date.getMonth()+1) + '-' + date.getDate();
-
-        this.setState({dateSelected: date});
-
-        fetch(url).then((response) => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                this.setState({error: {isActive: true, message: 'Date must be between Jun 16, 1995 and Apr 29, 2021'}});
-                throw new Error('Date must be between Jun 16, 1995 and Apr 29, 2021');
-            }
-        })
-        .then(data => {this.setState({apod: data})})
-        .catch((error) => {
-            console.log(error)
-        });
-    }
-
-    onAlertClose() {
-        this.setState({error: {isActive: false, message: ''}});
     }
     
     render() {
@@ -60,26 +24,12 @@ export default class Apod extends Component {
                     <DatePicker
                         className={'form-control'}
                         selected={this.state.dateSelected}
-                        onChange={(date) => this.handleDateChange(date)}
+                        onChange={(date) => this.setState({dateSelected: date})}
                     />
                     </div>
                 </div>
                 <hr></hr>
-                <Card style={{width: '18rem'}}>
-                    {this.state.apod.media_type === 'image' ? 
-                        <Card.Img variant="top" src={this.state.apod.url} /> 
-                        : 
-                        <Video src={this.state.apod.url}></Video>
-                    }
-                    <Card.Body>
-                        <Card.Title>{this.state.apod.title}</Card.Title>
-                        <Card.Text className="truncate">
-                            {this.state.apod.explanation}
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer className="text-muted">{this.state.apod.date}</Card.Footer>
-                </Card>
-                <Alert show={this.state.error.isActive} message={this.state.error.message} onClose={() => {this.onAlertClose()}}/>
+                <CardShow date={this.state.dateSelected.getFullYear() + '-' + (this.state.dateSelected.getMonth()+1) + '-' + this.state.dateSelected.getDate()} />
             </div>
         )
     }
---
+++ session-1/sample/src/components/apod/cardshow.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import Card from 'react-bootstrap/Card'
+import Video from '../video';
+import { API_KEY } from '../../config';
+
+export default class CardShow extends Component {  
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            apod: {}
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(this.props.date !== prevProps.date) {
+            const url = 'https://api.nasa.gov/planetary/apod?api_key=' + API_KEY + '&date=' + this.props.date;
+
+            fetch(url).then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    const message = 'Date must be between Jun 16, 1995 and Apr 29, 2021';
+                    throw new Error(message);
+                }
+            })
+            .then(data => this.setState({apod: data}))
+            .catch((error) => {
+                console.log(error)
+            });
+        }
+    }
+    
+    render() {
+        
+
+        return (
+            <Card style={{width: '18rem'}}>
+                {this.state.apod.media_type === 'image' ? 
+                    <Card.Img variant="top" src={this.state.apod.url} /> 
+                    : 
+                    <Video src={this.state.apod.url}></Video>
+                }
+                <Card.Body>
+                    <Card.Title>{this.state.apod.title}</Card.Title>
+                    <Card.Text className="truncate">
+                        {this.state.apod.explanation}
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer className="text-muted">{this.state.apod.date}</Card.Footer>
+            </Card>
+        )
+    }
+}
