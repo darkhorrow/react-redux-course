cde1586e976cf7ad8e898f449d80c3ab97b390fe
benearo.paez <benearo.paez@bitboxonline.com>
2021-05-03 09:34:52 +0100

    Added Router

--- session-1/sample/src/index.js
+++ session-1/sample/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
@@ -10,23 +8,41 @@ import Calculator from './components/calculator';
 import Heroes from './components/heroes';
 import Apod from './components/apod/apod';
 import Beers from './components/beer/beer';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Tabs defaultActiveKey="heroes" id="uncontrolled-tab-example">
-      <Tab eventKey="calculator" title="Calculator">
-        <Calculator />
-      </Tab>
-      <Tab eventKey="heroes" title="Heroes">
-        <Heroes />
-      </Tab>
-      <Tab eventKey="apod" title="Apod">
-        <Apod />
-      </Tab>
-      <Tab eventKey="beer" title="Beers">
-        <Beers />
-      </Tab>
-    </Tabs>
+    <Router>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand>React Course Exercises</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link><Link to="/calculator" className="nav-link">Calculator</Link></Nav.Link>
+              <Nav.Link><Link to="/heroes" className="nav-link">Heroes</Link></Nav.Link>
+              <Nav.Link><Link to="/apod" className="nav-link">Apod</Link></Nav.Link>
+              <Nav.Link><Link to="/" className="nav-link">Beers</Link></Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <Switch>
+          <Route path="/calculator">
+            <Calculator />
+          </Route>
+          <Route path="/heroes">
+            <Heroes />
+          </Route>
+          <Route path="/apod">
+            <Apod />
+          </Route>
+          <Route path="/">
+            <Beers />
+          </Route>
+        </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
